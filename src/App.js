c8b499fc8bcdf151c4e6ae3f3640b3d3584b3dc9
Mira Makadia <67718364+Mira85@users.noreply.github.com>
2021-12-31 16:06:32 -0500

    adds functionality

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
-// IMPORT useState
-import React from 'react';
-import './styles.css';
-// IMPORT imageData.js AND STORE IT IN A VARIABLE CALLED imagesArr
+import React from "react";
+import { useState } from "react";
+import "./styles.css";
+
+import imagesArr from "./imageData";
 
 export default function App() {
-  // USE useState TO CREATE  [bigImage, setBigImage] 
-  // AND SET IT TO THE IMAGE URL OF THE FIRST ELEMENT IN THE ARRAY
+  const [bigImage, setbigImage] = useState(
+    "https://images.unsplash.com/photo-1505761671935-60b3a7427bad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
+  );
 
-  // CREATE A HANDLE CLICK FUNCTION THAT ACCEPTS AN IMAGE URL
-  // THE FUNCTION SHOULD CALL setBigImage AND PASS IT THE URL
+  function HandleClick(image) {
+    setbigImage(image);
+  }
 
-  // CREATE A VARIABLE CALLED images THAT LOOPs OVER THE imagesArr AND RETURNS AN <IMG> ELEMENT
-  // ASSIGN ALL OF THE PROPERTIES THAT IT NEEDS: src, alt, className, key INCLUDING AN onClick EVENT THAT CALLS THE HANDLE EVENT FUNCTION AND PASSES IT THE IMG URL
+  const images = imagesArr.map((object, arrayIdx) => {
+    return (
+      <span
+        style={
+          object.img === bigImage
+            ? { border: "solid green" }
+            : { border: "none" }
+        }
+      >
+        {" "}
+        <img
+          className="thumb"
+          key={arrayIdx}
+          src={object.img}
+          alt={object.city}
+          onClick={() => HandleClick(object.img)}
+        />
+      </span>
+    );
+  });
 
   return (
     <div className="App">
       <h1>Cities Of The World</h1>
       <div id="wrapper">
-        <div id="thumbnails">
-          {/* RENDER THE IMAGES ARRAY  */}
-        </div>
+        <div id="thumbnails">{/* RENDER THE IMAGES ARRAY  */ images}</div>
         {/* THE SRC IMAGE URL SHOULD BE SET TO THE VALUE THAT IS STORED IN bigImage */}
-        {/* <img src="" id="bigimage" alt='bigImage'/> */}
+        {<img src={bigImage} id="bigimage" alt="bigImage" />}
       </div>
     </div>
   );
